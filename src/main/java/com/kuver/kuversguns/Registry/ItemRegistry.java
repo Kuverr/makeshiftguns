d1653c6d6d71d3388c2da716f73a9499b5c55d28
Kuver <ziutek8921@gmail.com>
2023-01-12 20:58:53 +0100

    asd

---
+++ src/main/java/com/kuver/kuversguns/Registry/ItemRegistry.java
@@ -0,0 +1,16 @@
+package com.kuver.kuversguns.Registry;
+
+import com.kuver.kuversguns.KuversGuns;
+import com.mrcrayfish.guns.GunMod;
+import com.mrcrayfish.guns.item.AmmoItem;
+import com.mrcrayfish.guns.item.GunItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemRegistry {
+    public static final DeferredRegister<Item> REGISTER = DeferredRegister.create(ForgeRegistries.ITEMS, KuversGuns.MOD_ID);
+
+    public static final RegistryObject<GunItem> TEST_GUN = REGISTER.register("test_gun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
+}
