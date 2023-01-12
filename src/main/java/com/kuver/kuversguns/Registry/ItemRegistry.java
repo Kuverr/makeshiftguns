a8acb616c4f23f3dd1a340bbae170e53374151ec
Kuver <ziutek8921@gmail.com>
2023-01-12 23:22:36 +0100

    asd

--- src/main/java/com/kuver/kuversguns/Registry/ItemRegistry.java
+++ src/main/java/com/kuver/kuversguns/Registry/ItemRegistry.java
@@ -13,4 +13,5 @@ public class ItemRegistry {
     public static final DeferredRegister<Item> REGISTER = DeferredRegister.create(ForgeRegistries.ITEMS, KuversGuns.MOD_ID);
 
     public static final RegistryObject<GunItem> TEST_GUN = REGISTER.register("test_gun", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
+    public static final RegistryObject<GunItem> BENELLIM4 = REGISTER.register("benellim4", () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
 }
